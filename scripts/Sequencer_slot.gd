1c85ec7139bb5d48e4d65034ee77db74b447b2d7
WillYum <liamshere98@gmail.com>
2024-01-27 22:45:35 +0200

    Implement logic for displaying events on sequencer slots

---
+++ scripts/Sequencer_slot.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@export var animated_sprites: Array[AnimatedSprite2D] = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass;
+
+
+func add_sprites(eventTypes: Array[EventSpawner.Event_Type]):
+	var anim_names = eventTypes.map(func(value: EventSpawner.Event_Type):
+		match value:
+			EventSpawner.Event_Type.EVENT_TYPE_BALLS:
+				return "Balls"
+			EventSpawner.Event_Type.EVENT_TYPE_BIRD:
+				return "Bird";
+			EventSpawner.Event_Type.EVENT_TYPE_BANANA:
+				return "Banana";
+	);
+
+	for i in range(anim_names.size()):
+		var selected_animation = animated_sprites[i];
+		selected_animation.play(anim_names[i]);
+	
+	pass;
